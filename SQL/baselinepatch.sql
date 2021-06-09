ece9c23efff3532eab8b2e4430fd47c19e554543
sibiyalifa <sibiyalifa@gmail.com>
2021-06-09 10:43:13 +0200

    modified stored proc for variance calculations

--- SQL/baselinepatch.sql
+++ SQL/baselinepatch.sql
@@ -11,7 +11,7 @@ CREATE TABLE [dbo].[tbl_Tolerance]
 (
 	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[itemGroup] VARCHAR(20) NOT NULL,
-	[tolerance] DECIMAL(18,9) NOT NULL
+	[tolerance] DECIMAL(18,7) NOT NULL
 )
 
 
@@ -30,7 +30,7 @@ IF (OBJECT_ID('[dbo].[fn_GetTolerance]') IS NOT NULL)
 GO
 
 CREATE FUNCTION [dbo].[fn_GetTolerance](@itemGroup VARCHAR(20))
-RETURNS  DECIMAL(18,9)
+RETURNS  DECIMAL(18,7)
 BEGIN
 RETURN (SELECT [tolerance] FROM [dbo].[tbl_Tolerance] WHERE [itemGroup] = @itemGroup)
 END
@@ -44,13 +44,13 @@ IF (OBJECT_ID('[dbo].[fn_CalculateVariance]') IS NOT NULL)
 	DROP FUNCTION [dbo].[fn_CalculateVariance]
 GO
 
-CREATE FUNCTION [dbo].[fn_CalculateVariance](@diff FLOAT, @tolerance DECIMAL(18,9))
-RETURNS FLOAT
+CREATE FUNCTION [dbo].[fn_CalculateVariance](@diff FLOAT, @tolerance DECIMAL(18,5))
+RETURNS VARCHAR(max)
 BEGIN
-DECLARE @variance FLOAT
+DECLARE @variance VARCHAR(MAX)
 
-	IF @diff < @tolerance
-		SET @variance = 0
+	IF @diff <= @tolerance
+		SET @variance = '0'
 	ELSE
 		SET @variance = @diff
 RETURN @variance
@@ -64,12 +64,15 @@ IF (OBJECT_ID('[dbo].[fn_GetDifference]') IS NOT NULL)
 	DROP FUNCTION [dbo].[fn_GetDifference]
 GO
 
-CREATE FUNCTION [dbo].[fn_GetDifference](@count1 DECIMAL(18,9), @sysCount DECIMAL(18,9))
-RETURNS DECIMAL(18,9)
+CREATE FUNCTION [dbo].[fn_GetDifference](@count1 DECIMAL(18,5), @sysCount DECIMAL(18,5))
+RETURNS DECIMAL(18,2)
 BEGIN
-DECLARE @diff DECIMAL(18,9)
+DECLARE @diff DECIMAL(18,7)
 
-	SET @diff = @count1 - @sysCount
+	IF @count1 >= @sysCount
+		SET @diff = @count1 - @sysCount
+	ELSE
+		SET @diff = @sysCount - @count1
 RETURN @diff
 END
 GO
@@ -77,24 +80,6 @@ GO
 
 
 
-
-
-
---select [dbo].[fn_CalculateVariance](0.002, 0.00001)
-
-
-
-
-
-select [dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](-0.019,-0.019), -0.02) AS Variance
-
-
-
-
-
-
-
---select [dbo].[fn_GetDifference](1303.699,1303.697)
 
 
 
