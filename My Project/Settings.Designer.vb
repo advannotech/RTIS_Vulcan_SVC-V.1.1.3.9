9c299a1eefea9db95de74ae5fa0b7711e0f87bd1
sibiyalifa <sibiyalifa@gmail.com>
2021-08-17 13:12:48 +0200

    sql views to round off to 5 decimal place

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -164,30 +164,6 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("CAT_RTIS_Test")>  _
-        Public Property RTDB() As String
-            Get
-                Return CType(Me("RTDB"),String)
-            End Get
-            Set
-                Me("RTDB") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Cataler_SCN_Test")>  _
-        Public Property EvoDB() As String
-            Get
-                Return CType(Me("EvoDB"),String)
-            End Get
-            Set
-                Me("EvoDB") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.DefaultSettingValueAttribute("CATALER2")>  _
         Public Property RTServer() As String
             Get
@@ -219,6 +195,30 @@ Namespace My
             End Get
             Set
                 Me("EvoComServer") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("CAT_RTIS")>  _
+        Public Property RTDB() As String
+            Get
+                Return CType(Me("RTDB"),String)
+            End Get
+            Set
+                Me("RTDB") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Cataler_SCN")>  _
+        Public Property EvoDB() As String
+            Get
+                Return CType(Me("EvoDB"),String)
+            End Get
+            Set
+                Me("EvoDB") = value
             End Set
         End Property
     End Class
--- SQL/baselinepatch.sql
+++ SQL/baselinepatch.sql
@@ -1,4 +1,4 @@
-USE CAT_RTIS_Test
+USE CAT_RTIS
 GO
 
 
@@ -109,7 +109,7 @@ WHEN(il.[fCountQty] - il.[fSystemQty]) < 0 THEN il.[fCountQty] END DESC
 
 
 
--------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 
 
 
@@ -119,32 +119,326 @@ WHERE [iInvCountID] = 131
 
 -- update the variance expression
 UPDATE [RTIS_InvCountLines]
-SET [fCountQty] = 1.95123   ,
-[fCountQty2] = 1.95123   ,
+SET [fCountQty] = 1.87123  ,
+[fCountQty2] = 1.87123  ,
 [fSystemQty] = 2
-WHERE [idInvCountLines] = 326
+WHERE [idInvCountLines] = 341
 
 
 
 
---------------------------------
+-----------------------------------------------------------------------------------------------------
 
 
 
+SELECT * FROM [RTIS_InvCountLines]
+WHERE [iInvCountID] = 131
+
+
+-- update the variance expression
 UPDATE [RTIS_InvCountLines]
-SET [fCountQty] = 2,
-[fCountQty2] = 2,
-[fSystemQty] = 1.98      
-WHERE [idInvCountLines] = 326
+SET [fCountQty] = 2  ,
+[fCountQty2] = 2  ,
+[fSystemQty] = 1.95123  
+WHERE [idInvCountLines] = 341
 
 
---------------------------------
 
---IF (@diff BETWEEN -(@tolerance) AND @tolerance) AND (@diff NOT IN (-(@tolerance),@tolerance))
-IF (0.05 BETWEEN -(0.05) AND 0.05) AND (0.05 NOT IN (-(0.05),0.05))
-	SELECT 'Unacceptable'
-ELSE
-	SELECT 'Acceptable'
+
+-----------------------------------------------------------------------------------------------------
+
+
+
+
+--Items below system count
+IF (OBJECT_ID('[dbo].[vw_GetItemsBelowSystemCount]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetItemsBelowSystemCount]
+GO
+
+CREATE VIEW [dbo].[vw_GetItemsBelowSystemCount]
+AS
+SELECT s.[Code] AS [ItemCode],
+i.[cInvCountNo],
+s.[Description_1] AS [Desc] ,
+b.[cBinLocationName] AS Bin ,
+ROUND(il.[fCountQty], 5)  AS [Counted], 
+ROUND(il.[fCountQty2], 5) AS [Counted2],
+ROUND(il.[fSystemQty], 5) AS [System], 
+ROUND(CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup])) AS VARCHAR(50)), 5) AS [Variance] ,
+w.[Code] AS [WHSE]  ,
+i.[dPrepared] 
+FROM [RTIS_InvCountLines] il 
+INNER JOIN [RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID] 
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID] 
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID] 
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON b.[idBinLocation] = il.[iBinLocationId] 
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking] 
+WHERE il.[fSystemQty] > il.[fCountQty]  
+AND il.[fCountQty] <> 0 
+AND il.[fCountQty] = il.[fCountQty2] 
+AND il.[bOnST] = 1 
+--ORDER BY s.[Code]
+GO
+
+
+
+
+--SELECT * from [vw_GetItemsBelowSystemCount]
+--WHERE [vw_GetItemsBelowSystemCount].[cInvCountNo] = 'STK0182'
+--ORDER BY [vw_GetItemsBelowSystemCount].[ItemCode]
+
+
+
+
+
+--Items Above system count
+IF (OBJECT_ID('[dbo].[vw_GetItemsAboveSystemCount]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetItemsAboveSystemCount]
+GO
+
+CREATE VIEW [dbo].[vw_GetItemsAboveSystemCount]
+AS
+SELECT s.[Code] AS [ItemCode], 
+i.[cInvCountNo],
+s.[Description_1] AS [Desc] , 
+b.[cBinLocationName] AS Bin ,
+ROUND(il.[fCountQty], 5)  AS [Counted], 
+ROUND([fCountQty2], 5) AS [Counted2] ,
+ROUND(il.[fSystemQty], 5) AS [System], 
+ROUND(CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup])) AS VARCHAR(50)), 5) AS [Variance] ,
+w.[Code] AS [WHSE] ,
+i.[dPrepared] 
+FROM [RTIS_InvCountLines] il 
+INNER JOIN [RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID] 
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID] 
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID] 
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON b.[idBinLocation] = il.[iBinLocationId] 
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking] 
+--WHERE i.[cInvCountNo] = 'STK0182' 
+WHERE il.[fSystemQty] < il.[fCountQty] 
+AND il.[fCountQty] = il.[fCountQty2] 
+AND il.[bOnST] = 1 
+--ORDER BY s.[Code]
+GO
+
+
+
+SELECT * FROM [dbo].[vw_GetItemsAboveSystemCount] WHERE [vw_GetItemsAboveSystemCount].cInvCountNo = 'STK0182' ORDER BY [vw_GetItemsAboveSystemCount].[ItemCode]
+
+
+
+
+--Uncounted Items
+IF (OBJECT_ID('[dbo].[vw_GetItemsUncounted]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetItemsUncounted]
+GO
+
+CREATE VIEW [dbo].[vw_GetItemsUncounted]
+AS
+SELECT s.[Code] AS [ItemCode] ,  
+i.[cInvCountNo],
+s.[Description_1] AS [Desc] , 
+b.[cBinLocationName] AS Bin ,
+'0'  AS [Counted],  
+ROUND([fCountQty2], 5) AS [Counted2] ,
+ROUND(il.[fSystemQty], 5) AS [System], 
+ROUND(CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup])) AS VARCHAR(50)), 5) AS [Variance] ,
+w.[Code] AS [WHSE] ,
+i.[dPrepared] 
+FROM [RTIS_InvCountLines] il 
+INNER JOIN [RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID] 
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID] 
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID] 
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON b.[idBinLocation] = il.[iBinLocationId] 
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking] 
+--WHERE i.[cInvCountNo] = 'STK0182' 
+WHERE (il.[fCountQty] = 0 OR il.[fCountQty] IS NULL OR il.[fCountQty] = '') 
+AND (il.[fCountQty2] = 0 OR il.[fCountQty2] IS NULL OR il.[fCountQty2] = '') 
+AND (il.[fCountQty] <> 0 OR il.[fCountQty2] <> 0 OR il.[fSystemQty] <> 0) 
+AND il.[bOnST] = 1 
+--ORDER BY s.[Code]
+GO
+
+
+
+
+SELECT * FROM [dbo].[vw_GetItemsUncounted] WHERE [vw_GetItemsUncounted].cInvCountNo = 'STK0182' ORDER BY [vw_GetItemsUncounted].[ItemCode]
+
+
+
+
+
+
+
+--Items equal to system count
+IF (OBJECT_ID('[dbo].[vw_GetItemsEqualSystemCount]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetItemsEqualSystemCount]
+GO
+
+CREATE VIEW [dbo].[vw_GetItemsEqualSystemCount]
+AS
+SELECT s.[Code] AS [ItemCode],  
+i.[cInvCountNo],
+s.[Description_1] AS [Desc], 
+b.[cBinLocationName] AS Bin ,
+ROUND(il.[fCountQty], 5)  AS [Counted], 
+ROUND([fCountQty2], 5) AS [Counted2] ,
+ROUND(il.[fSystemQty], 5) AS [System], 
+ROUND(CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup])) AS VARCHAR(50)), 5) AS [Variance] ,
+w.[Code] AS [WHSE] ,
+i.[dPrepared] 
+FROM [RTIS_InvCountLines] il 
+INNER JOIN [RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID] 
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID] 
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID] 
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON b.[idBinLocation] = il.[iBinLocationId] 
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking] 
+--WHERE i.[cInvCountNo] = 'STK0140' 
+WHERE il.[fCountQty] = il.[fSystemQty] 
+AND il.[fCountQty] <> 0 
+AND il.[fSystemQty] <> 0 
+AND il.[fCountQty] = il.[fCountQty2] 
+AND il.[bOnST] = 1 
+--ORDER BY s.[Code]
+GO
+
+
+
+
+
+
+
+SELECT * FROM [dbo].[vw_GetItemsEqualSystemCount] WHERE [vw_GetItemsEqualSystemCount].[cInvCountNo] = 'STK0140' ORDER BY [vw_GetItemsEqualSystemCount].[ItemCode]
+
+
+
+
+
+
+
+--Unqual scanner quantities
+IF (OBJECT_ID('[dbo].[vw_GetUnequalScannerQty]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetUnequalScannerQty]
+GO
+
+CREATE VIEW [dbo].[vw_GetUnequalScannerQty]
+AS
+SELECT s.[Code] AS [ItemCode], 
+i.[cInvCountNo],
+s.[Description_1] AS [Desc] , 
+b.[cBinLocationName] AS Bin ,
+ROUND(il.[fCountQty], 5)  AS [Counted], 
+ROUND([fCountQty2], 5) AS [Counted2] ,
+ROUND(il.[fSystemQty], 5) AS [System], 
+ROUND(CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup])) AS VARCHAR(50)), 5) AS [Variance] ,
+w.[Code] AS [WHSE] ,
+i.[dPrepared] 
+FROM [RTIS_InvCountLines] il 
+INNER JOIN [RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID] 
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID] 
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID] 
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON b.[idBinLocation] = il.[iBinLocationId] 
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking] 
+--WHERE i.[cInvCountNo] = 'STK0182' 
+WHERE il.[fCountQty] <> il.[fCountQty2] 
+AND il.[bOnST] = 1 
+--ORDER BY s.[Code]
+GO
+
+
+
+
+
+
+
+SELECT * FROM [dbo].[vw_GetUnequalScannerQty] WHERE [vw_GetUnequalScannerQty].[cInvCountNo] = 'STK0182' ORDER BY [vw_GetUnequalScannerQty].[ItemCode]
+
+
+
+
+
+--unlisted Items
+IF (OBJECT_ID('[dbo].[vw_GetUnlistedItems]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetUnlistedItems]
+GO
+
+CREATE VIEW [dbo].[vw_GetUnlistedItems]
+AS
+SELECT s.[Code] AS [ItemCode], 
+i.[cInvCountNo],
+s.[Description_1] AS [Desc], 
+b.[cBinLocationName] AS Bin ,
+ROUND(il.[fCountQty], 5)  AS [Counted], 
+ROUND([fCountQty2], 5) AS [Counted2] ,
+ROUND(il.[fSystemQty], 5) AS [System], 
+ROUND(CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup])) AS VARCHAR(50)), 5) AS [Variance] ,
+w.[Code] AS [WHSE] ,
+i.[dPrepared] 
+FROM [RTIS_InvCountLines] il 
+INNER JOIN [RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID] 
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID] 
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID] 
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON b.[idBinLocation] = il.[iBinLocationId] 
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking] 
+--WHERE i.[cInvCountNo] = 'STK0182' 
+WHERE il.[bOnST] = 0 
+--ORDER BY s.[Code]
+GO
+
+
+
+
+
+
+SELECT * FROM [dbo].[vw_GetUnlistedItems] WHERE [vw_GetUnlistedItems].[cInvCountNo] = 'STK0182' ORDER BY [vw_GetUnlistedItems].[ItemCode]
+
+
+
+
+
+--Stock take variance
+IF (OBJECT_ID('[dbo].[vw_GetStockTakeVariances]') IS NOT NULL)
+	DROP VIEW [dbo].[vw_GetStockTakeVariances]
+GO
+
+CREATE VIEW [dbo].[vw_GetStockTakeVariances]
+AS
+SELECT
+il.[idInvCountLines] AS [gclineID]
+,i.[cInvCountNo]
+,s.[Code] AS [gcItemCode]
+, s.[Bar_Code] AS [gcBarcode]
+, s.[Description_1] AS [gcItemDesc]
+, b.[cBinLocationName] AS [gcBin]
+, l.[cLotDescription] AS [gcLot]
+, ROUND(il.[fCountQty], 4) AS [gcCounted]
+, ROUND(il.[fCountQty2], 4) AS [gcCounted2]
+, ROUND(il.[fSystemQty], 4) AS [gcSystem]
+, CAST([dbo].[fn_CalculateVariance]([dbo].[fn_GetDifference](il.[fCountQty],il.[fSystemQty]), [dbo].[fn_GetTolerance](s.[ItemGroup]))AS VARCHAR(50)) AS [gcVarience]
+, w.[Code] AS [gcWhseCode]
+, w.[Name] AS [gcWhseName]
+, il.[bIsCounted] AS [gcIsCounted]
+, il.[bOnST] AS [gcOnST]
+FROM[RTIS_InvCountLines] il
+INNER JOIN[RTIS_InvCount] i ON i.[idInvCount] = [iInvCountID]
+INNER JOIN [Cataler_SCN].[dbo].[StkItem] s ON s.[StockLink] = il.[iStockID]
+INNER JOIN [Cataler_SCN].[dbo].[WhseMst] w ON w.[WhseLink] = il.[iWarehouseID]
+LEFT JOIN [Cataler_SCN].[dbo].[_etblLotTracking] l ON il.[iLotTrackingID] = l.[idLotTracking]
+LEFT JOIN [Cataler_SCN].[dbo].[_btblBINLocation] b ON il.[iBinLocationId] = b.[idBinLocation]
+--WHERE i.[cInvCountNo] = 'STK0182'  
+--ORDER BY
+--CASE WHEN(il.[fSystemQty] - il.[fCountQty]) < 0 THEN il.[fCountQty]
+--WHEN(il.[fCountQty] - il.[fSystemQty]) < 0 THEN il.[fCountQty] END DESC
+GO
+
+
+
+SELECT * FROM [dbo].[vw_GetStockTakeVariances] 
+WHERE [vw_GetStockTakeVariances].[cInvCountNo] = 'STK0182' 
+ORDER BY
+CASE WHEN([vw_GetStockTakeVariances].[gcSystem] - [vw_GetStockTakeVariances].[gcCounted]) < 0 THEN [vw_GetStockTakeVariances].[gcCounted]
+WHEN([vw_GetStockTakeVariances].[gcCounted] - [vw_GetStockTakeVariances].[gcSystem]) < 0 THEN [vw_GetStockTakeVariances].[gcCounted] END DESC
+
 
 
 
